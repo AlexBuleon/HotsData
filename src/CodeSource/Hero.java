ff1c705e09f62e4f295ecf8aa9329e0d661e150b
Antoine Chédin <antoine.chedin@gmail.com>
2015-09-16 19:56:16 +0200

    add Hero attribut

--- src/CodeSource/Hero.java
+++ src/CodeSource/Hero.java
@@ -1,6 +1,19 @@
 package CodeSource;
 
 public class Hero {
+		
+	public static final int ROLE_ASSASSIN = 0;
+	public static final int ROLE_WARRIOR = 1;
+	public static final int ROLE_SUPPORT = 2;
+	public static final int ROLE_SPECIALIST = 3;
+	
+	public static final int TYPE_MELEE = 0;
+	public static final int TYPE_RANGED = 1;
+	
 	private String name;
+	private int role;
+	private int type;
+	private Stat[] baseStats;
+	private Stat[] statsPerLevel;
 	
 }
