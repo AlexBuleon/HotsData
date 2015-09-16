c2d36b9214be51c7a17c2894f82c5845819026e1
Antoine Chédin <antoine.chedin@gmail.com>
2015-09-16 20:25:05 +0200

    add Constructor and getter

--- src/CodeSource/Hero.java
+++ src/CodeSource/Hero.java
@@ -10,10 +10,32 @@ public class Hero {
 	public static final int TYPE_MELEE = 0;
 	public static final int TYPE_RANGED = 1;
 	
+	
 	private String name;
 	private int role;
 	private int type;
 	private Stat[] stats;
 	
-	
+	public Hero (String name, int role, int type, Stat[] stats){
+		this.name = name;
+		this.role = role;
+		this.type = type;
+		this.stats = stats;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getRole() {
+		return role;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public Stat[] getStats() {
+		return stats;
+	}	
 }
