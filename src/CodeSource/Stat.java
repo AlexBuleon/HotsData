98cb679723790d42798bd6525fdbb026e797441f
AlexBuleon <alex.buleon@gmail.com>
2015-09-16 20:19:13 +0200

    Ajout class Stat

---
+++ src/CodeSource/Stat.java
@@ -0,0 +1,28 @@
+package CodeSource;
+
+public class Stat {
+	
+	private String name;
+	private double value;
+	private double valuePerLevel;
+	
+	public Stat(String name, double value, double valuePerLevel) {
+		
+		this.name = name;
+		this.value = value;
+		this.valuePerLevel = valuePerLevel;
+		
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getValue() {
+		return this.value;
+	}
+	
+	public double getValuePerLevel() {
+		return this.valuePerLevel;
+	}
+}
