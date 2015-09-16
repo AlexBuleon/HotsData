6cbcd42788a603efd2bc2a7ffed49f498bb3a857
AlexBuleon <alex.buleon@gmail.com>
2015-09-16 20:07:27 +0200

    fix d'une ligne inutile

--- src/CodeSource/HotsData.java
+++ src/CodeSource/HotsData.java
@@ -14,7 +14,6 @@ public class HotsData {
 				// get URL content
 
 				String a = "http://heroesofthestorm.gamepedia.com/Falstad?version=bf62a12727dbb685f3c639ac45595b0a";
-				http://heroesofthestorm.gamepedia.com/Falstad?version=bf62a12727dbb685f3c639ac45595b0a
 				url = new URL(a);
 				URLConnection conn = url.openConnection();
 				conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
